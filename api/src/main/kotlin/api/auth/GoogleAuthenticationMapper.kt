018813fb56fb6c22571670f9dcccc887d54870cc
Daniel Bastos <danmrbastos@gmail.com>
2022-07-08 22:54:45 -0300

    feat: add user ID to authentication attributes

--- api/src/main/kotlin/api/auth/GoogleAuthenticationMapper.kt
+++ api/src/main/kotlin/api/auth/GoogleAuthenticationMapper.kt
@@ -1,11 +1,12 @@
 package api.auth
 
 import api.auth.failures.AccountNotRegisteredResponse
-import api.people.service.PersonService
+import api.people.dal.dao.PersonRepository
 import io.micronaut.security.authentication.AuthenticationResponse
+import io.micronaut.security.config.AuthenticationModeConfiguration
+import io.micronaut.security.oauth2.configuration.OpenIdAdditionalClaimsConfiguration
 import io.micronaut.security.oauth2.endpoint.authorization.state.State
 import io.micronaut.security.oauth2.endpoint.token.response.DefaultOpenIdAuthenticationMapper
-import io.micronaut.security.oauth2.endpoint.token.response.OpenIdAuthenticationMapper
 import io.micronaut.security.oauth2.endpoint.token.response.OpenIdClaims
 import io.micronaut.security.oauth2.endpoint.token.response.OpenIdTokenResponse
 import jakarta.inject.Named
@@ -14,24 +15,30 @@ import jakarta.inject.Singleton
 @Singleton
 @Named("google")
 class GoogleAuthenticationMapper(
-  private val defaultMapper: DefaultOpenIdAuthenticationMapper,
-  private val personService: PersonService
-) : OpenIdAuthenticationMapper {
+  openIdAdditionalClaimsConfiguration: OpenIdAdditionalClaimsConfiguration,
+  authenticationModeConfiguration: AuthenticationModeConfiguration,
+  private val personService: PersonRepository
+) : DefaultOpenIdAuthenticationMapper(openIdAdditionalClaimsConfiguration, authenticationModeConfiguration) {
+
   override fun createAuthenticationResponse(
     providerName: String?,
     tokenResponse: OpenIdTokenResponse?,
     openIdClaims: OpenIdClaims?,
     state: State?
   ): AuthenticationResponse {
-    if (openIdClaims == null || openIdClaims.email == null) {
-      return AccountNotRegisteredResponse()
-    }
+    val email = openIdClaims?.email ?: return AccountNotRegisteredResponse()
 
-    val emailExists = personService.exists(openIdClaims.email)
+    val emailExists = personService.emailExists(email)
     if (!emailExists) {
-      return AccountNotRegisteredResponse(openIdClaims.name, openIdClaims.email)
+      return AccountNotRegisteredResponse(openIdClaims.name, email)
     }
 
-    return defaultMapper.createAuthenticationResponse(providerName, tokenResponse, openIdClaims, state)
+    val user = personService.findByEmail(email)
+    val roles = getRoles(providerName, tokenResponse, openIdClaims)
+    val username = getUsername(providerName, tokenResponse, openIdClaims)
+    val claims = buildAttributes(providerName, tokenResponse, openIdClaims)
+    user?.id?.let { claims.put("id", it.toString()) }
+
+    return AuthenticationResponse.success(username, roles, claims)
   }
 }
--- api/src/main/kotlin/api/people/dal/model/Person.kt
+++ api/src/main/kotlin/api/people/dal/model/Person.kt
@@ -4,16 +4,18 @@ import io.micronaut.core.annotation.Creator
 import io.micronaut.core.annotation.Introspected
 import org.bson.codecs.pojo.annotations.BsonCreator
 import org.bson.codecs.pojo.annotations.BsonProperty
+import org.bson.types.ObjectId
 import java.time.LocalDateTime
 
 @Introspected
-class Person @Creator @BsonCreator constructor(
+data class Person @Creator @BsonCreator constructor(
   @field:BsonProperty("fullName") @param:BsonProperty("fullName") val fullName: String? = null,
   @field:BsonProperty("displayName") @param:BsonProperty("displayName") var displayName: String? = null,
   @field:BsonProperty("email") @param:BsonProperty("email") val email: String,
   @field:BsonProperty("github") @param:BsonProperty("github") var github: String,
   @field:BsonProperty("linkedin") @param:BsonProperty("linkedin") var linkedin: String,
   @field:BsonProperty("skills") @param:BsonProperty("skills") var skills: String,
+  @field:BsonProperty("_id") @param:BsonProperty("_id") val id: ObjectId? = null,
   @field:BsonProperty("creationDate") @param:BsonProperty("creationDate") val creationDate: LocalDateTime =
     LocalDateTime.now()
 )
