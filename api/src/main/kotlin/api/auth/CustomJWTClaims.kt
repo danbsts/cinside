f4dad9b36f00bade49d68c8bd401a359998e1876
Daniel Bastos <danmrbastos@gmail.com>
2022-07-09 17:07:07 -0300

    fix: google oauth mapper with openid

--- api/src/main/kotlin/api/auth/CustomJWTClaims.kt
+++ api/src/main/kotlin/api/auth/CustomJWTClaims.kt
@@ -14,29 +14,22 @@ import org.slf4j.LoggerFactory
 @Singleton
 @Replaces(ClaimsGenerator::class)
 class CustomJWTClaims(
-  tokenConfiguration: TokenConfiguration,
-  jwtIdGenerator: JwtIdGenerator,
-  claimsAudienceProvider: ClaimsAudienceProvider,
-  applicationConfiguration: ApplicationConfiguration
-) : JWTClaimsSetGenerator(
-  tokenConfiguration,
-  jwtIdGenerator,
-  claimsAudienceProvider,
-  applicationConfiguration
-) {
+) : ClaimsGenerator {
   val LOG = LoggerFactory.getLogger(CustomJWTClaims::class.java)
 
   override fun generateClaims(authentication: Authentication?, expiration: Int?): MutableMap<String, Any> {
     LOG.info("CLAIMSS!! at CustomJWTClaims")
     LOG.info(authentication?.attributes.toString())
-    val claims = super.generateClaims(authentication, expiration)
-    LOG.info(claims.toString())
-    return claims
+//    LOG.info(claims.toString())
+    if (authentication != null) {
+      return authentication.attributes
+    }
+    return HashMap()
   }
 
   override fun generateClaimsSet(oldClaims: MutableMap<String, *>?, expiration: Int?): MutableMap<String, Any> {
     LOG.info("OLD CLAIMS")
     LOG.info(oldClaims.toString())
-    return super.generateClaimsSet(oldClaims, expiration)
+    return HashMap()
   }
 }
---
+++ api/src/main/kotlin/api/auth/GoogleOpenIdAuthenticationMapper.kt
@@ -0,0 +1,62 @@
+package api.auth
+
+import api.auth.failures.AccountNotRegisteredResponse
+import api.people.dal.dao.PersonRepository
+import io.micronaut.security.authentication.AuthenticationResponse
+import io.micronaut.security.oauth2.endpoint.authorization.state.State
+import io.micronaut.security.oauth2.endpoint.token.response.OauthAuthenticationMapper
+import io.micronaut.security.oauth2.endpoint.token.response.OpenIdAuthenticationMapper
+import io.micronaut.security.oauth2.endpoint.token.response.OpenIdClaims
+import io.micronaut.security.oauth2.endpoint.token.response.OpenIdTokenResponse
+import io.micronaut.security.token.jwt.generator.claims.JwtClaims
+import jakarta.inject.Named
+import jakarta.inject.Singleton
+import org.bson.types.ObjectId
+import org.slf4j.LoggerFactory
+import java.util.*
+import java.util.function.Consumer
+
+@Singleton
+@Named("google")
+class GoogleOpenIdAuthenticationMapper(
+  private val personService: PersonRepository
+) : OpenIdAuthenticationMapper {
+  val LOG = LoggerFactory.getLogger(GoogleOpenIdAuthenticationMapper::class.java)
+
+  override fun createAuthenticationResponse(
+    providerName: String?,
+    tokenResponse: OpenIdTokenResponse?,
+    openIdClaims: OpenIdClaims?,
+    state: State?
+  ): AuthenticationResponse {
+    val email = openIdClaims?.email ?: return AccountNotRegisteredResponse()
+
+    val emailExists = personService.emailExists(email)
+    if (!emailExists) {
+      return AccountNotRegisteredResponse(openIdClaims.name, email)
+    }
+
+    val user = personService.findByEmail(email)
+    LOG.info(user.toString())
+    val claims = buildAttributes(providerName, tokenResponse, openIdClaims, user?.id)
+    LOG.info(claims.toString())
+    return AuthenticationResponse.success(openIdClaims.subject, Collections.emptyList(), claims)
+  }
+
+  private fun buildAttributes(
+    providerName: String?,
+    tokenResponse: OpenIdTokenResponse?,
+    openIdClaims: OpenIdClaims,
+    userId: ObjectId?
+  ): Map<String, Any> {
+    val claims: MutableMap<String, Any> = HashMap(openIdClaims.claims)
+    JwtClaims.ALL_CLAIMS.forEach(Consumer { key: String -> claims.remove(key) })
+    tokenResponse?.let {
+      claims.put(OpenIdAuthenticationMapper.OPENID_TOKEN_KEY, it.idToken)
+    }
+    providerName?.let { claims[OauthAuthenticationMapper.PROVIDER_KEY] = it }
+    userId?.let { claims["id"] = it }
+    return claims
+  }
+
+}
