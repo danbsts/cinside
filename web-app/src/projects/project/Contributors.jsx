f0178f5085773841d5df2f56911e0de9aa8fa589
Daniel Bastos <danmrbastos@gmail.com>
2022-07-31 01:31:40 -0300

    refactor: remove unecessary code from contributors box

--- web-app/src/projects/project/Contributors.jsx
+++ web-app/src/projects/project/Contributors.jsx
@@ -10,7 +10,10 @@ import removeIcon from 'assets/remove-cross.svg';
 
 const contributorStyle = css`
   margin-bottom: 24px;
-  margin-right: 30px;
+  width: 40%;
+  @media(min-width: 400px) {
+    width: 25%;
+  }
 `;
 const roleStyle = css`
   font-size: 16px;
@@ -22,29 +25,20 @@ const contributorsContainerStyle = css`
 `;
 
 function ContributorBox({ name, remove, role }) {
-  if (!remove) {
-    return (
-      <div className={contributorStyle}>
-        <DktText holder="h4">{name}</DktText>
-        <DktText holder="p" style={roleStyle}>{role}</DktText>
-      </div>
-    );
-  }
-
   return (
-    <FlexLayout width="fit-content">
-      <div className={contributorStyle}>
+    <FlexLayout style={contributorStyle}>
+      <div>
         <DktText holder="h4">{name}</DktText>
         <DktText holder="p" style={roleStyle}>{role}</DktText>
       </div>
-      <ClickableIcon alt="Remove" src={removeIcon} onClick={() => remove(name)} />
+      {remove && <ClickableIcon alt="Remove" src={removeIcon} onClick={() => remove(name)} />}
     </FlexLayout>
   );
 }
 
 export default function Contributors({ contributors, remove }) {
   return (
-    <FlexLayout style={contributorsContainerStyle}>
+    <FlexLayout justifyContent="space-between" style={contributorsContainerStyle}>
       {contributors.map(({ name, role }, idx) => (
         // eslint-disable-next-line react/no-array-index-key
         <ContributorBox key={idx} name={name} remove={remove} role={role} />
